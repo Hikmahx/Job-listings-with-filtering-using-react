bccfb019926640d0c30e6f8835c5e071909e56ea
Hikmahx <hikmayousuph@gmail.com>
2021-09-21 13:28:25 +0100

    filterjs imported

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header';
+import Filter from './components/Filter'
 import Jobs from './components/jobs/Jobs';
 
 function App() {
   return <div className='App'>
     <Header />
+    <Filter/>
     <Jobs />
   </div>;
 }
