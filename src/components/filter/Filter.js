49e04e666c6aba922e83f4220fe4921de7c1fd92
Hikmahx <hikmayousuph@gmail.com>
2021-09-21 20:25:24 +0100

    filterBtnjs component added and imported

--- src/components/filter/Filter.js
+++ src/components/filter/Filter.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import FilterBtn from './FilterBtn';
 
 function Filter() {
   return (
     <div className='filter-container'>
-      <div className='selected-btn'></div>
+      <div className='selected-btn'>
+        <FilterBtn/>
+      </div>
       <p className='clear'>Clear</p>
     </div>
   );
