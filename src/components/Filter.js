e916ffe01e394eda7ec87558ef54a9318a345567
Hikmahx <hikmayousuph@gmail.com>
2021-09-21 13:23:35 +0100

    fitler.js JSX added

---
+++ src/components/Filter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Filter() {
+  return (
+    <div class='filter-container'>
+      <div class='selected-btn'></div>
+      <p class='clear'>Clear</p>
+    </div>
+  );
+}
+
+export default Filter;
