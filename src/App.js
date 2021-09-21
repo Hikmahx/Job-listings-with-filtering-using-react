8328fe41cf53ac3f6eabe9f8f5825d0e0323a04c
Hikmahx <hikmayousuph@gmail.com>
2021-09-21 14:16:26 +0100

    state transferred to appjs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
+import React, {useState, useEffect } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Filter from './components/Filter'
 import Jobs from './components/jobs/Jobs';
 
 function App() {
+  useEffect(() => {
+    getData();
+  }, []);
+  
+  const [jobs, setJobs] = useState([]);
+  
+  const getData = async () =>{
+    const data = await fetch('./data.json');
+
+    const jobs = await data.json();
+
+    setJobs(jobs);
+  }
+  
   return <div className='App'>
     <Header />
     <main>
       <Filter/>
-      <Jobs />
+      <Jobs jobs = {jobs} />
     </main>
   </div>;
 }
