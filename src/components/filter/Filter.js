d75cd96598ee46045afa063fdec74fb18329dfbd
Hikmahx <hikmayousuph@gmail.com>
2021-09-22 19:26:06 +0100

    onclick added and clearAllBtns added as props

--- src/components/filter/Filter.js
+++ src/components/filter/Filter.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import FilterBtn from './FilterBtn';
 
-const Filter = ({ selectedBtn, removeBtn }) => {
+const Filter = ({ selectedBtn, removeBtn, clearAllBtns }) => {
   return (
     <div className='filter-container'  style ={selectedBtn.length > 0? {display:'block'} :{display:'none'}} >
       <div className='selected-btn'>
-          <FilterBtn btns={selectedBtn} removeBtn= {removeBtn} />
+          <FilterBtn btns={selectedBtn} removeBtn= {removeBtn}  />
       </div>
-      <p className='clear'>Clear</p>
+      <p className='clear' onClick = {clearAllBtns}>Clear</p>
     </div>
   )
 }
