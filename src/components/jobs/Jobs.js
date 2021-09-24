290ac7912c5ebb88a07521e6d3170918f8eb5bdd
Hikmahx <hikmayousuph@gmail.com>
2021-09-24 19:15:03 +0100

    displayCard fxn for if it contains each of the selectedBtns

--- src/components/jobs/Jobs.js
+++ src/components/jobs/Jobs.js
@@ -1,18 +1,35 @@
 import React, { Fragment} from 'react';
 import JobItem from './JobItem';
 
-const Jobs = ({jobs, click}) => {
+const Jobs = ({jobs, click, selectedBtn}) => {
 
   const itemList = item => {
     return <button onClick={click} data-tools={item}>{item}</button>;
   };
   
 
+  const displayCard = (job) =>{
+    let jobId = ''
+
+    if(selectedBtn.length > 0 ){
+      let jobBtnArray = Array.from([job.role, job.level, ...job.tools, ...job.languages])
+
+      selectedBtn.every(btn=> jobBtnArray.includes(btn))? jobId = {display: 'block'} : jobId =  {display: 'none'}
+      console.log(job.id)
+      return jobId
+    }else{
+      jobId = {display: 'block'}
+      console.log(job.id)
+      return jobId
+    } 
+}
+
   return (
     <Fragment>
     <ul className='jobs'>
       {jobs.map(job => (
         <JobItem
+          displayCard = {displayCard(job)}
           key={job.id}
           company={job.company}
           logo={job.logo}
