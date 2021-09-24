d61f2244b52caf6534cb0c516e8032bf1ba23358
Hikmahx <hikmayousuph@gmail.com>
2021-09-24 22:56:59 +0100

    ref added to link

--- src/components/Attribution.js
+++ src/components/Attribution.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Attribution = () =>{
     return (
       <div className="attribution" role="contentinfo">
-        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" style={{color: '#5ba4a4'}}>Frontend Mentor</a>. 
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noopener noreferrer" style={{color: '#5ba4a4'}}>Frontend Mentor</a>. 
         Coded by <a href="https://github.com/Hikmahx" style={{color: '#5ba4a4'}}>Hikmah</a>.
       </div>
     )
