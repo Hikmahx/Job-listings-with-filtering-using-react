0000b766be8618302884c4353c7e22a5c009397a
Hikmah Yousuph <54153841+Hikmahx@users.noreply.github.com>
2022-04-15 01:52:16 -1100

    Update Attribution.js

--- src/components/Attribution.js
+++ src/components/Attribution.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Attribution = () =>{
     return (
-      <div className="attribution" role="contentinfo">
+      <div className="attribution">
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noopener noreferrer" style={{color: '#5ba4a4'}}>Frontend Mentor</a>. 
         Coded by <a href="https://github.com/Hikmahx" style={{color: '#5ba4a4'}}>Hikmah</a>.
       </div>
